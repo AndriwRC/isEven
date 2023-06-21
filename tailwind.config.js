813b062429d99418fe31e95dd4430e24f1385b29
AndriwRC <randriw4@gmail.com>
2023-06-20 23:38:34 -0500

    basic dark mode layout & styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ['./src/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            dropShadow: {
+                'title-1': [
+                    '-4px -4px 12px rgba(59, 130, 246, 0.3)',
+                    '4px 4px 12px rgba(250, 250, 250, 0.3)',
+                ],
+                'success-1': '0px 0px 24px rgba(74, 222, 128, 0.4)',
+            },
+            minWidth: {
+                328: '328px',
+            },
+            minHeight: {
+                256: '256px',
+            },
+        },
     },
     plugins: [],
 };
